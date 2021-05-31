213421ad240e9c6db4e4fd4ddd1543fe42a226c5
Ildar Gilfanov <gilfanov.ildar@gmail.com>
2021-05-31 16:01:00 +0300

    Add Shimmer Framework

---
+++ Shimmer/Shimmer.h
@@ -0,0 +1,19 @@
+//
+//  Shimmer.h
+//  Shimmer
+//
+//  Created by Ildar Gilfanov on 31.05.2021.
+//  Copyright © 2021 Kimon Tsinteris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shimmer.
+FOUNDATION_EXPORT double ShimmerVersionNumber;
+
+//! Project version string for Shimmer.
+FOUNDATION_EXPORT const unsigned char ShimmerVersionString[];
+
+#import <Shimmer/FBShimmering.h>
+#import <Shimmer/FBShimmeringView.h>
+#import <Shimmer/FBShimmeringLayer.h>
